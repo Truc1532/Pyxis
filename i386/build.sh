cba80bb57e507b68a8517c9338e6cbad8d6a2fc1
Arthur <165717741+Truc1532@users.noreply.github.com>
2025-02-06 16:26:10 -0400

    Update build.sh

--- i386/build.sh
+++ i386/build.sh
@@ -1,13 +1,22 @@
+BUILD_DIR="build"
+SRC_DIR="src"
+
+if [ -d "$BUILD_DIR" ]; then
+	rm -rf build/*
+else
+	mkdir "$BUILD_DIR"
+fi
+
 # compiling
 echo "Compiling..."
-nasm -f elf32 -o build/loader.o src/assembly/loader.asm
-gcc -m32 -o build/main.o -c src/main.c
-gcc -m32 -o build/time.o -c src/time.c
+nasm -f elf32 -o "$BUILD_DIR/loader.o" "$SRC_DIR/assembly/loader.asm"
+gcc -m32 -o "$BUILD_DIR/main.o" -c "$SRC_DIR/main.c"
+gcc -m32 -o "build/time.o" -c "src/time.c"
 
 # linking
 echo "Linking..."
-gcc -m32 -r build/time.o build/main.o -o build/kernel.o
-ld -T src/linker/linker.ld -m elf_i386 -o build/pyxis.bin build/kernel.o
+gcc -m32 -r "$BUILD_DIR/time.o" "$BUILD_DIR/main.o" -o "$BUILD_DIR/kernel.o"
+ld -T src/linker/linker.ld -m elf_i386 -o "$BUILD_DIR/pyxis.bin" "$BUILD_DIR/kernel.o"
 
 # creating the iso
 mkdir -p isodir/boot/grub/
