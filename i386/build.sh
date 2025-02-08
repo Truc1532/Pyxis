773ea809380a75c91239ddd0a1e012eef727de34
Arthur <165717741+Truc1532@users.noreply.github.com>
2025-02-08 05:34:20 -0400

    Update build.sh

--- i386/build.sh
+++ i386/build.sh
@@ -11,11 +11,12 @@ fi
 echo "Compiling..."
 nasm -f elf32 -o "$BUILD_DIR/loader.o" "$SRC_DIR/assembly/loader.asm"
 gcc -m32 -o "$BUILD_DIR/main.o" -c "$SRC_DIR/main.c"
-gcc -m32 -o "build/time.o" -c "src/time.c"
+gcc -m32 -o "$BUILD_DIR/time.o" -c "$SRC_DIR/time.c"
+gcc -m32 -o "$BUILD_DIR/text.o" -c "$SRC_DIR/text.c"
 
 # linking
 echo "Linking..."
-gcc -m32 -r "$BUILD_DIR/time.o" "$BUILD_DIR/main.o" -o "$BUILD_DIR/kernel.o"
+gcc -m32 -r "$BUILD_DIR/text.o" "$BUILD_DIR/time.o" "$BUILD_DIR/main.o" -o "$BUILD_DIR/kernel.o"
 ld -T "$SRC_DIR/linker/linker.ld" -m elf_i386 -o "$BUILD_DIR/pyxis.bin" "$BUILD_DIR/loader.o" "$BUILD_DIR/kernel.o"
 
 # creating the iso
