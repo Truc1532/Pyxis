420066d13818296c90ee25864a97b3e4615f28db
Arthur <165717741+Truc1532@users.noreply.github.com>
2026-03-01 23:00:11 +0100

    Refactor build script to use variables for compiler

--- amd64/build.sh
+++ amd64/build.sh
@@ -1,20 +1,22 @@
 SRC_DIR="src"
 BUILD_DIR="build"
+CC=x86_64-linux-gnu-gcc
+LD=x86_64-linux-gnu-ld
 
 echo "Compiling..."
 nasm -f elf64 -o "$BUILD_DIR/boot.o" "$SRC_DIR/assembly/boot.asm"
 nasm -f elf64 -o "$BUILD_DIR/boot64.o" "$SRC_DIR/assembly/boot64.asm"
 nasm -f elf64 -o "$BUILD_DIR/header.o" "$SRC_DIR/assembly/header.asm"
 # nasm -f elf64 -o idt.o src/assembly/idt.asm
-x86_64-linux-gnu-gcc -o "$BUILD_DIR/main.o" -c "$SRC_DIR/main.c" -nostdlib -nostartfiles -nodefaultlibs
-x86_64-linux-gnu-gcc -o "$BUILD_DIR/commands.o" -c "$SRC_DIR/commands.c" -nostdlib -nostartfiles -nodefaultlibs
-x86_64-linux-gnu-gcc -o "$BUILD_DIR/time.o" -c "$SRC_DIR/time.c" -nostdlib -nostartfiles -nodefaultlibs
-x86_64-linux-gnu-gcc -o "$BUILD_DIR/cpuinfo.o" -c "$SRC_DIR/cpuinfo.c" -nostdlib -nostartfiles -nodefaultlibs
-x86_64-linux-gnu-gcc -o "$BUILD_DIR/strings.o" -c "$SRC_DIR/strings.c" -nostdlib -nostartfiles -nodefaultlibs
+$CC -o "$BUILD_DIR/main.o" -c "$SRC_DIR/main.c" -nostdlib -nostartfiles -nodefaultlibs
+$CC -o "$BUILD_DIR/commands.o" -c "$SRC_DIR/commands.c" -nostdlib -nostartfiles -nodefaultlibs
+$CC -o "$BUILD_DIR/time.o" -c "$SRC_DIR/time.c" -nostdlib -nostartfiles -nodefaultlibs
+$CC -o "$BUILD_DIR/cpuinfo.o" -c "$SRC_DIR/cpuinfo.c" -nostdlib -nostartfiles -nodefaultlibs
+$CC -o "$BUILD_DIR/strings.o" -c "$SRC_DIR/strings.c" -nostdlib -nostartfiles -nodefaultlibs
 
 echo "Linking..."
-x86_64-linux-gnu-gcc -r "$BUILD_DIR/time.o" "$BUILD_DIR/strings.o" "$BUILD_DIR/cpuinfo.o" "$BUILD_DIR/commands.o" "$BUILD_DIR/main.o" -o "$BUILD_DIR/kernel.o" -nostdlib -nostartfiles -nodefaultlibs
-x86_64-linux-gnu-ld -T "$SRC_DIR/linker/linker.ld" -o "$BUILD_DIR/kernel.efi" "$BUILD_DIR/boot.o" "$BUILD_DIR/boot64.o" "$BUILD_DIR/header.o" "$BUILD_DIR/kernel.o"
+$CC -r "$BUILD_DIR/time.o" "$BUILD_DIR/strings.o" "$BUILD_DIR/cpuinfo.o" "$BUILD_DIR/commands.o" "$BUILD_DIR/main.o" -o "$BUILD_DIR/kernel.o" -nostdlib -nostartfiles -nodefaultlibs
+$LD -T "$SRC_DIR/linker/linker.ld" -o "$BUILD_DIR/kernel.efi" "$BUILD_DIR/boot.o" "$BUILD_DIR/boot64.o" "$BUILD_DIR/header.o" "$BUILD_DIR/kernel.o"
 
 
 mkdir -p isodir/boot/grub/
