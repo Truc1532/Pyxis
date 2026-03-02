99e02ff6e979f658113d7a17e244719a74d2d623
Arthur <165717741+Truc1532@users.noreply.github.com>
2026-03-02 18:16:51 +0100

    Refactor build script for aarch64 architecture

--- aarch64/build.sh
+++ aarch64/build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-ASM_DIR=src/assembly
-RS_DIR=src
-BUILD_DIR=build
+SRC_DIR=src/
+BUILD_DIR=build/
+AS=aarch64-linux-gnu-as
+CC=aarch64-linux-gnu-gcc
+LD=aarch64-linux-gnu-ld
 
 set -e
 
-aarch64-linux-gnu-as $ASM_DIR/boot.S -o build/boot.o
+$AS -o $BUILD_DIR/boot.o $SRC_DIR/boot.S
+$AS -o $BUILD_DIR/vectors.o $SRC_DIR/vector_tb.S
+$CC -o $BUILD_DIR/main.o -c $SRC_DIR/main.c -ffreestanding -O2
+$CC -o $BUILD_DIR/uart.o -c $SRC_DIR/uart.c -ffreestanding -O2
+$CC -o $BUILD_DIR/mem.o -c $SRC_DIR/mem.c -ffreestanding -O2
 
-rustc $RS_DIR/main.rs --target aarch64-unknown-none -C opt-level=2 -C panic=abort --emit=obj -o $BUILD_DIR/main.o
+$LD -T linker.ld -o kernel.elf $BUILD_DIR/boot.o $BUILD_DIR/vectors.o $BUILD_DIR/main.o $BUILD_DIR/uart.o $BUILD_DIR/mem.o
 
-aarch64-linux-gnu-ld -T linker.ld $BUILD_DIR/boot.o $BUILD_DIR/main.o -o kernel.elf
-
-qemu-system-aarch64 -m 128M -M virt -cpu cortex-a53 -nographic -serial mon:stdio -kernel kernel.elf
+./run.sh
