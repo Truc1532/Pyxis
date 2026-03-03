0754aec23e76b6e3e459647f499e322531fa87cd
Arthur <165717741+Truc1532@users.noreply.github.com>
2026-03-03 23:04:42 +0100

    Update build script to use Rust for main module

--- aarch64/build.sh
+++ aarch64/build.sh
@@ -2,18 +2,16 @@
 
 ASM_DIR=src/arch
 BUILD_DIR=build
+
 AS=aarch64-linux-gnu-as
-CC=aarch64-linux-gnu-gcc
-LD=aarch64-linux-gnu-ld
 
 set -e
 
-$AS -o $BUILD_DIR/boot.o $ASM_DIR/boot.S
-$AS -o $BUILD_DIR/vectors.o $ASM_DIR/vector_tb.S
-$CC -o $BUILD_DIR/main.o -c src/main.c -ffreestanding -O2
-$CC -o $BUILD_DIR/uart.o -c src/uart.c -ffreestanding -O2
-$CC -o $BUILD_DIR/mem.o -c src/mem.c -ffreestanding -O2
+$AS $ASM_DIR/boot.S -o build/boot.o
+$AS $ASM_DIR/vector_tb.S -o build/vectors.o
 
-$LD -T linker.ld -o kernel.elf $BUILD_DIR/boot.o $BUILD_DIR/vectors.o $BUILD_DIR/main.o $BUILD_DIR/uart.o $BUILD_DIR/mem.o
+rustc src/main.rs --target aarch64-unknown-none -C opt-level=2 -C panic=abort --emit=obj -o $BUILD_DIR/main.o
+
+aarch64-linux-gnu-ld -T linker.ld $BUILD_DIR/boot.o $BUILD_DIR/vectors.o $BUILD_DIR/main.o -o kernel.elf
 
 ./run.sh
