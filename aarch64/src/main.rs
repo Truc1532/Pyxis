51fd2c2ea40e9395d3452118b25c6c20fa651655
Arthur <165717741+Truc1532@users.noreply.github.com>
2026-03-03 23:06:16 +0100

    Update and rename main.c to main.rs

---
+++ aarch64/src/main.rs
@@ -0,0 +1,60 @@
+#![no_main] 
+#![no_std]
+
+use core::arch::asm;
+use core::panic::PanicInfo;
+
+mod uart;
+mod mem;
+
+
+#[no_mangle]
+pub extern "C" fn kernel_main(el: u64) -> ! {
+    uart::uart_put_ptr(el as *const u8);
+    uart::uart_puts("\nTHIS CODE IS RUNNING IN RUST\r\n");
+
+    extern "C" {
+        static __heap_start: u8;
+        static __heap_end: u8;
+    }
+
+    let mut allocator = mem::BumpAllocator {
+        heap_start: unsafe { &__heap_start as *const _ as usize },
+        heap_end: unsafe { &__heap_end as *const _ as usize },
+        next: unsafe { &__heap_start as *const _ as usize },
+        
+    };
+
+    let ptr = allocator.alloc(100, 8); 
+    let ptr2 = allocator.alloc(3, 8);
+
+    uart::uart_put_ptr(ptr);
+    uart::uart_put_ptr(ptr2);
+
+    let test = 0xdead_beef as *const u8;
+
+    uart::uart_put_ptr(test);
+
+	uart::uart_puts("\n");
+
+	unsafe { 
+        asm!("svc #0"); 
+    } 
+    
+    loop {
+        unsafe { 
+            asm!("wfe"); 
+        }
+    }
+
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    uart::uart_puts("PANICKED!!!");
+    loop {
+        unsafe { 
+            asm!("wfe"); 
+        }
+    }
+}
