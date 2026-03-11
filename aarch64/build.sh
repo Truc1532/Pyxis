446b5913bea5584b4221347159be1805c69c0a54
Arthur <165717741+Truc1532@users.noreply.github.com>
2026-03-11 11:47:06 +0100

    Update build.sh

--- aarch64/build.sh
+++ aarch64/build.sh
@@ -14,4 +14,7 @@ rustc src/main.rs --target aarch64-unknown-none -C opt-level=2 -C panic=abort --
 
 aarch64-linux-gnu-ld -T linker.ld $BUILD_DIR/boot.o $BUILD_DIR/vectors.o $BUILD_DIR/main.o -o kernel.elf
 
+#~/.rustup/toolchains/stable-aarch64-unknown-linux-gnu/lib/rustlib/aarch64-unknown-none/lib/libcore-*.rlib \
+#~/.rustup/toolchains/stable-aarch64-unknown-linux-gnu/lib/rustlib/aarch64-unknown-none/lib/libcompiler_builtins-*.rlib \
+
 ./run.sh
